73adf4934e6b874ee56e87a1c71703ccaa8a3968
Seiya_Satoh <satohseiya0312@gmail.com>
2021-04-21 08:42:15 +0900

    プロジェクト ファイルを追加します。

---
+++ TodoApp/Global.asax.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TodoApp.Models;
+using TodoApp.Migrations;
+
+namespace TodoApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TodoesContext, Migrations.Configuration>());
+        }
+    }
+}
